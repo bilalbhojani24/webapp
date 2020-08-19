f1c2324a0d2f5452bc26039f19db8639721866e4
Bilal Bhojani <bilalbhojani24@gmail.com>
2020-08-20 02:27:18 +0530

    bug fix 1

--- src/Card.jsx
+++ src/Card.jsx
@@ -6,7 +6,7 @@ function Card(props) {
     <>
       <div className="col-md-4 col-10 mx-auto">
         <div className="card">
-          <img src={props.imgsrc} alt="image" className="card-img-top" style={{height:"250px"}}/>
+          <img src={props.imgsrc} alt="cardpic" className="card-img-top" style={{height:"250px"}}/>
           <div className="card-body">
             <h5 className="card-title">{props.title}</h5>
             <p className="card-text">
